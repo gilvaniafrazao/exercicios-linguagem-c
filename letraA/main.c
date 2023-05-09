a9ba66bf23c7c3d1ec7b1070860ff9949788d838
gilvânia <113179901+gilvaniafrazao@users.noreply.github.com>
2023-05-09 08:13:57 -0300

    Add files via upload

---
+++ letraA/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// resolução alternativa A//
+int main(void)
+{
+    float c, f;
+    printf("C = "); scanf("%f", &c);
+    f = (9 * c + 160) / 5;
+    printf("F = %f", f);
+    return(0);
+}
